63350029f27ff6665acc3f3c7bc87ec1d80e8e4e
admin <admin@DESKTOP-RE5E17I>
2023-01-11 09:13:12 +0900

    23.01.11 project start

---
+++ WHLProject/src/main/java/com/example/ArkETC/WhlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArkETC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhlProjectApplication.class, args);
+	}
+
+}
---
+++ WHLProject/src/test/java/com/example/ArkETC/WhlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArkETC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
