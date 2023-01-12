6c25e60920797f79bde926295a774f3604a545a8
admin <admin@DESKTOP-RE5E17I>
2023-01-12 14:00:59 +0900

    23.01.12 Product Auto Login 기능 추가

--- WHLProject/src/main/Product/ProductLogin.js
+++ WHLProject/src/main/Product/ProductLogin.js
@@ -1,5 +1,20 @@
 // 제품 page 로그인 기능
+
+// 자동 로그인 check 확인
+function productLoginCheck(){
+	if(sessionStorage!=null){
+		return productLoginDataSet();
+	}else{
+		loginPwEncoding();
+		
+		loginPwDecoding(id, pw);
+		productLoginCheck(id, pw);
+	}
+}
+
 function loginPwEncoding(id, pw){
+	sessionStorage.clear();
+	
 	const idEncoded = encodeURI(id);
 	const pwEncoded = encodeURI(pw);
 	console.log(idEncoded, pwEncoded);
@@ -19,6 +34,17 @@ function productLoginCheck(idDecoded, pwDecoded){
 		console.log(" login failed ");
 		return alert(" login failed ");
 	}
+}
+
+// 자동 로그인 체크 시
+function productAutoLogin(idDecoded, pwDecoded){
+	sessionStorage.setItem("id",idDecoded);
+	sessionStorage.setItem("pw",pwDecoded);
+}
+
+// 로그인 후 데이터를 page에 전송 - 상단 이름 부분에 아이디 출력
+function productLoginDataSet(){
+	return sessionStorage.getItem(id);
 }
 
 // 상품 추가, 삭제, 변경
